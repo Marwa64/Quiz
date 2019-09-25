78c8b5d0b300a10d1bed35bcf8b250026bf22474
Marwa64 <46577944+Marwa64@users.noreply.github.com>
2019-09-25 22:13:45 +0200

    Update Quiz Game.cpp

--- Quiz Game.cpp
+++ Quiz Game.cpp
@@ -7,64 +7,64 @@
 
 using namespace std;
 
-class Question{
-	public:
-		// This is just the constructor that takes values by val and assigns them to private variables in the class
-		Question(string question, string correctChoice, string choice2, string choice3, string choice4, int ID)
+class Question {
+public:
+	// This is just the constructor that takes values by val and assigns them to private variables in the class
+	Question(string question, string correctChoice, string choice2, string choice3, string choice4, int ID)
 		: _question(question), _correctChoice(correctChoice), _choice2(choice2), _choice3(choice3), _choice4(choice4), _ID(ID)
-		{
+	{
 
+	}
+
+	// This function takes a parameter and compares it to the correct solution of the question
+	bool CheckAnswer(string answer) {
+		if (answer == _correctChoice) {
+			return true;
 		}
-
-		// This function takes a parameter and compares it to the correct solution of the question
-		bool CheckAnswer(string answer) {
-			if (answer == _correctChoice) {
-				return true;
-			}
-			else {
-				return false;
-			}
+		else {
+			return false;
 		}
+	}
 
-		// This function prints out the question
-		void PrintQuestion() {
-			cout << _question << endl;
+	// This function prints out the question
+	void PrintQuestion() {
+		cout << _question << endl;
+	}
+
+	// This function prints out the choices depending on a random number
+	void PrintChoices(int n) {
+		switch (n) {
+		case 1:
+			cout << _correctChoice;
+			break;
+		case 2:
+			cout << _choice2;
+			break;
+		case 3:
+			cout << _choice3;
+			break;
+		case 4:
+			cout << _choice4;
+			break;
 		}
+	}
 
-		// This function prints out the choices depending on a random number
-		void PrintChoices(int n) {
-			switch (n) {
-				case 1:
-					cout << _correctChoice;
-					break;
-				case 2:
-					cout << _choice2;
-					break;
-				case 3:
-					cout << _choice3;
-					break;
-				case 4:
-					cout << _choice4;
-					break;
-			}
-		}
+	// This function returns the ID of the question
+	int GetID() {
+		return _ID;
+	}
 
-		// This function returns the ID of the question
-		int GetID() {
-			return _ID;
-		}
-
-	private:
-		string _question;
-		string _correctChoice;
-		string _choice2;
-		string _choice3;
-		string _choice4;
-		int _ID;
+private:
+	string _question;
+	string _correctChoice;
+	string _choice2;
+	string _choice3;
+	string _choice4;
+	int _ID;
 };
 
 // This function reads the text file, makes the questions and its choices into objects, then addes them to the vector
-void loadQuestions(int &lastID, vector<Question> &allQuestions, string filename) {
+void loadQuestions(int& lastID, vector<Question>& allQuestions, string filename) {
 	string temp, question, correctC, c2, c3, c4;
 	int line = 1;
 
@@ -73,25 +73,25 @@ void loadQuestions(int &lastID, vector<Question> &allQuestions, string filename)
 
 	while (getline(QFile, temp)) {
 		switch (line) {
-			case 1:
-				question = temp;
-				break;
-			case 2:
-				correctC = temp;
-				break;
-			case 3:
-				c2 = temp;
-				break;
-			case 4:
-				c3 = temp;
-				break;
-			case 5:
-				c4 = temp;
-				lastID++;
-				Question newQuestion(question, correctC, c2, c3,c4, lastID);
-				allQuestions.push_back(newQuestion);
-				line = 0;
-				break;
+		case 1:
+			question = temp;
+			break;
+		case 2:
+			correctC = temp;
+			break;
+		case 3:
+			c2 = temp;
+			break;
+		case 4:
+			c3 = temp;
+			break;
+		case 5:
+			c4 = temp;
+			lastID++;
+			Question newQuestion(question, correctC, c2, c3, c4, lastID);
+			allQuestions.push_back(newQuestion);
+			line = 0;
+			break;
 		}
 		line++;
 	}
@@ -117,10 +117,10 @@ void ViewAllQuestions(vector<Question> allQuestions) {
 
 		while (done == false) {
 			num = rand() % 4 + 1;
-			if (choices[num-1] == false) {
+			if (choices[num - 1] == false) {
 				allQuestions[i].PrintChoices(num);
 				cout << "   ";
-				choices[num-1] = true; // minus 1 here because of the index
+				choices[num - 1] = true; // minus 1 here because of the index
 			}
 			for (int j = 0; j < 4; j++) {
 				done = true;
@@ -135,16 +135,16 @@ void ViewAllQuestions(vector<Question> allQuestions) {
 }
 
 // This function will delete a question from the vector
-void DeleteQuestion(vector<Question> &allQuestions) {
+void DeleteQuestion(vector<Question>& allQuestions) {
 	int ID;
 	cout << "Enter the question number: ";
 	cin >> ID;
 	allQuestions.erase(allQuestions.begin() + ID - 1); // begin() gets the 1st value in the vector 
-	cout << endl << "Question " << ID << " has been deleted" << endl;;
+	cout << endl << "Question " << ID << " has been deleted" << endl;
 }
 
 // This function will add a new question to the vector
-void AddQuestion(int &lastID, vector<Question> &allQuestions) {
+void AddQuestion(int& lastID, vector<Question>& allQuestions) {
 	string question, c1, c2, c3, c4;
 	cout << endl << "Enter the question: ";
 	cin.ignore();
@@ -162,7 +162,7 @@ void AddQuestion(int &lastID, vector<Question> &allQuestions) {
 	allQuestions.push_back(newQuestion);
 }
 
-void adminMenu(int &lastID, vector<Question> &allQuestions) {
+void adminMenu(int& lastID, vector<Question>& allQuestions) {
 	int choice;
 	string nameEntered, fileName;
 	char input;
@@ -172,35 +172,35 @@ void adminMenu(int &lastID, vector<Question> &allQuestions) {
 	cout << "Your choice: ";
 	cin >> choice;
 	switch (choice) {
-		case 1:
+	case 1:
+		ViewAllQuestions(allQuestions);
+		cout << endl << " ---------------------------------------";
+		cout << endl << "Enter d to delete a question and b to go back to the main menu: ";
+		cin >> input;
+		switch (input) {
+		case 'D':
+		case 'd':
+			DeleteQuestion(allQuestions);
+			cout << " ---------------------------------------" << endl;
 			ViewAllQuestions(allQuestions);
-			cout << endl << " ---------------------------------------";
-			cout << endl << "Enter d to delete a question and b to go back to the main menu: ";
-			cin >> input;
-			switch (input) {
-				case 'D':
-				case 'd':
-					DeleteQuestion(allQuestions);
-					cout << " ---------------------------------------" << endl;
-					ViewAllQuestions(allQuestions);
-					break;
+			break;
 
-				case 'B':
-				case 'b':
-					break;
-			}
+		case 'B':
+		case 'b':
 			break;
-		case 2:
-			AddQuestion(lastID, allQuestions);
-			break;
-		case 3:
-			cout << "Enter the name of the file: ";
-			cin >> nameEntered;
-			fileName = nameEntered + ".txt";
-			loadQuestions(lastID, allQuestions, fileName);
-			break;
-		case 4:
-			break;
+		}
+		break;
+	case 2:
+		AddQuestion(lastID, allQuestions);
+		break;
+	case 3:
+		cout << "Enter the name of the file: ";
+		cin >> nameEntered;
+		fileName = nameEntered + ".txt";
+		loadQuestions(lastID, allQuestions, fileName);
+		break;
+	case 4:
+		break;
 	}
 }
 
